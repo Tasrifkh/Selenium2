ecd0cca699e34b691e52041020b1088b2e2ecefc
Tasrifkh <tasrif.me@gmail.com>
2023-08-06 11:37:59 +0600

    Pop up screen verify

---
+++ popup.py
@@ -0,0 +1,44 @@
+# import time
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# "Authentication pop up can not be handled by using any locators in selenium."
+# "We need to pass username:password pair in application URL."
+#
+#
+# driver = webdriver.Chrome()
+# driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth")
+#
+# time.sleep(2)
+#
+# driver.quit()
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Set the path to the ChromeDriver executable
+chrome_driver_path = "/Users/nqveenkhunteta/Downloads/chromedriver"
+# Set the URL with basic authentication
+url_with_auth = "http://admin:admin@the-internet.herokuapp.com/basic_auth"
+
+# Configure ChromeDriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-extensions")
+driver = webdriver.Chrome()
+
+try:
+    # Open the URL with basic authentication
+    driver.get(url_with_auth)
+    time.sleep(2)
+    # Find and print the page message
+    page_message = driver.find_element(By.CSS_SELECTOR, "p").text
+    print(page_message)
+
+finally:
+    # Close the browser window
+    driver.quit()
+
+
+
+
